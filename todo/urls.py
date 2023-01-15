070e32eaaf7e264cf614a22adfa4b43a13c57fd6
baggi <lyesbabas0@gmail.com>
2023-01-15 21:29:02 +0100

    create add todo api

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
-from .views import RegisterView
+from .views import RegisterView,TodoView
 from django.urls import path
 
 urlpatterns = [
-    path("register/",RegisterView.as_view(),name="register_user")
+    path("register/",RegisterView.as_view(),name="register_user"),
+    path("add_todo/",TodoView.as_view(),name="create_todo")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer,TodoSerializer
 from rest_framework import status
 
 from rest_framework.response import Response
@@ -11,5 +11,15 @@ class RegisterView(APIView):
         user_serializer.is_valid(raise_exception=True)
         user_serializer.save()
         return Response(status=status.HTTP_201_CREATED)
+
+class TodoView(APIView):
+    def post(self,request):
+        todo_serializer=TodoSerializer(data=request.data)
+        todo_serializer.is_valid(raise_exception=True)
+        todo_serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+
+
 
 
