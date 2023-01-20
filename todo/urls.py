b7d432be0f7cda62f820129f9d8a22e8507a0953
baggi <lyesbabas0@gmail.com>
2023-01-20 13:52:17 +0100

    delete django version

--- todo/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import RegisterView,TodoView
-from django.urls import path
-
-urlpatterns = [
-    path("register/",RegisterView.as_view(),name="register_user"),
-    path("add_todo/",TodoView.as_view(),name="create_todo")
-]
